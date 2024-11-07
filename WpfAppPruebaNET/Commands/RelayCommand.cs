0d1f2e3971962b71ef6ac753af0b0006cdb7f9d3
Amed <Amed@DESKTOP-1DTLDH3>
2024-11-07 17:10:18 +0100

    Agregar archivos de proyecto.

---
+++ WpfAppPruebaNET/Commands/RelayCommand.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfAppPruebaNET.Commands
+{
+    /// <summary>
+    /// Relay Command class
+    /// </summary>
+    public class RelayCommand : ICommand
+    {
+        private readonly Func<bool> _canExecute;
+        private readonly Action _execute;
+
+        public RelayCommand(Action execute)
+            : this(execute, null)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="execute"></param>
+        /// <param name="canExecute"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parameter"></param>
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
---
+++ WpfAppPruebaNET/ViewModels/CronometroWatchViewModel.xaml.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using WpfAppPruebaNET.Commands;
+namespace WpfAppPruebaNET.ViewModels
+{
+    /// <summary>
+    /// Lógica de interacción para CronometroWatchViewModel.xaml
+    /// </summary>
+    public class CronometroViewModel : ViewModelBase
+    {
+        private TimeSpan _elapsedTime;
+        private bool _isRunning;
+
+        public TimeSpan ElapsedTime
+        {
+            get { return _elapsedTime; }
+            set
+            {
+                _elapsedTime = value;
+                OnPropertyChanged("ElapsedTime");
+                OnPropertyChanged("TimeDisplay");
+            }
+        }
+
+        public string TimeDisplay => $"Cronómetro : {ElapsedTime.Hours:D2}:{ElapsedTime.Minutes:D2}:{ElapsedTime.Seconds:D2}";
+
+        private ICommand _startCommand;
+        public ICommand StartCommand
+        {
+            get
+            {
+                if (_startCommand == null)
+                {
+                    _startCommand = new RelayCommand(StartStopwatch, CanStartStopwatch);
+                }
+                return _startCommand;
+            }
+        }
+
+        private ICommand _pauseCommand;
+        public ICommand PauseCommand
+        {
+            get
+            {
+                if (_pauseCommand == null)
+                {
+                    _pauseCommand = new RelayCommand(PauseStopwatch, CanPauseStopwatch);
+                }
+                return _pauseCommand;
+            }
+        }
+
+        private ICommand _stopCommand;
+        public ICommand StopCommand
+        {
+            get
+            {
+                if (_stopCommand == null)
+                {
+                    _stopCommand = new RelayCommand(StopStopwatch, CanStopStopwatch);
+                }
+                return _stopCommand;
+            }
+        }
+
+        private System.Windows.Threading.DispatcherTimer _timer;
+
+        /// <summary>
+        /// Stop watch ViewModel
+        /// </summary>
+        public CronometroViewModel()
+        {
+            _timer = new System.Windows.Threading.DispatcherTimer();
+            _timer.Tick += Timer_Tick;
+            _timer.Interval = TimeSpan.FromSeconds(1);
+        }
+
+        /// <summary>
+        /// Start / Stop watch
+        /// </summary>
+        private void StartStopwatch()
+        {
+            _isRunning = true;
+            _timer.Start();
+        }
+
+        /// <summary>
+        /// Can Start / Stop watch
+        /// </summary>
+        /// <returns></returns>
+        private bool CanStartStopwatch()
+        {
+            return !_isRunning;
+        }
+
+        /// <summary>
+        /// Pause Stop watch
+        /// </summary>
+        private void PauseStopwatch()
+        {
+            _isRunning = false;
+            _timer.Stop();
+        }
+
+        /// <summary>
+        /// Can Pause Stop watch
+        /// </summary>
+        /// <returns></returns>
+        private bool CanPauseStopwatch()
+        {
+            return _isRunning;
+        }
+
+        /// <summary>
+        /// Stop / Stop watch
+        /// </summary>
+        private void StopStopwatch()
+        {
+            _isRunning = false;
+            _timer.Stop();
+            ElapsedTime = TimeSpan.Zero;
+        }
+
+        /// <summary>
+        /// Can Stop Stop watch
+        /// </summary>
+        /// <returns></returns>
+        private bool CanStopStopwatch()
+        {
+            return _elapsedTime != TimeSpan.Zero || _isRunning;
+        }
+        /// <summary>
+        /// Event Timer Tick 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            ElapsedTime += TimeSpan.FromSeconds(1);
+        }
+    }
+}
